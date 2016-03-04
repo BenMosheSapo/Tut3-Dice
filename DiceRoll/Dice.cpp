87d79131ee06354c3fa63a10522200aefd858c98
BenMosheSapo <bensapo@gmail.com>
2016-03-04 16:22:07 +0200

    Added override average function with integer array parameter

---
+++ DiceRoll/Dice.cpp
@@ -0,0 +1,45 @@
+#include "Dice.h"
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+Dice::Dice()
+{
+}
+
+
+int Dice::Roll(){
+	return rand()%6 +1;
+}
+
+Dice::~Dice()
+{
+}
+
+float average(Dice Die, int NumRoll){
+	int i;
+	float sum=0;
+	for (i = 0; i < NumRoll; i++){
+		sum += Die.Roll();
+		cout << sum << endl;
+	}
+	return sum / NumRoll;
+}
+float average(int Rolls[], int NumRoll){
+	int i;
+	float sum = 0;
+	for (i = 0; i < NumRoll; i++){
+		sum += Rolls[i];
+	}
+	return sum / NumRoll;
+}
+
+int main(){
+	int Rolls[2] = { 1, 2 };
+	float test;
+	Dice die;
+	test = average(Rolls, 2);
+	cout << test << endl;
+	test = average(die, 2);
+	cout << test;
+}
